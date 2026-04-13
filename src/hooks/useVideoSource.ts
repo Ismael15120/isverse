37260411f0b1831549e84919428ba6ec023dcca7
Ismael Diawara <ismaeldiawara1er@gmail.com>
2026-04-13 08:59:37 +0000

    fix: force French (VF) audio language in streaming sources

--- src/hooks/useVideoSource.ts
+++ src/hooks/useVideoSource.ts
@@ -20,25 +20,29 @@ function buildSources(
   // Sources classées : VF/VOSTFR en priorité, puis sources génériques
   if (isMovie) {
     return [
-      { name: 'Videasy (VF)', url: `https://player.videasy.net/movie/${tmdbId}?language=fr` },
-      { name: 'VidSrc Pro',   url: `https://vidsrc.pro/embed/movie/${tmdbId}?lang=fr` },
-      { name: 'VidSrc.to',    url: `https://vidsrc.to/embed/movie/${tmdbId}?lang=fr` },
-      { name: '2Embed',       url: `https://www.2embed.cc/embed/${tmdbId}?lang=fr` },
-      { name: 'Embed.su',     url: `https://embed.su/embed/movie/${tmdbId}` },
-      { name: 'MovieBox',     url: `https://moviesapi.club/movie/${tmdbId}` },
-      { name: 'SuperEmbed',   url: `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1` },
-      { name: 'AutoEmbed',    url: `https://autoembed.co/movie/tmdb/${tmdbId}` },
+      { name: 'Frembed (VF)',   url: `https://frembed.cyou/embed/movie/${tmdbId}` },
+      { name: 'Videasy (VF)',   url: `https://player.videasy.net/movie/${tmdbId}?language=fr` },
+      { name: 'VidSrc.me (VF)', url: `https://vidsrc.me/embed/movie/${tmdbId}?ds_lang=fr` },
+      { name: 'VidSrc Pro',     url: `https://vidsrc.pro/embed/movie/${tmdbId}?lang=french` },
+      { name: 'VidSrc.to',      url: `https://vidsrc.to/embed/movie/${tmdbId}?lang=french` },
+      { name: '2Embed',         url: `https://www.2embed.cc/library/movie/${tmdbId}?lang=fr` },
+      { name: 'Embed.su',       url: `https://embed.su/embed/movie/${tmdbId}?lang=french` },
+      { name: 'MovieBox',       url: `https://moviesapi.club/movie/${tmdbId}` },
+      { name: 'SuperEmbed',     url: `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1&lang=french` },
+      { name: 'AutoEmbed',      url: `https://autoembed.co/movie/tmdb/${tmdbId}` },
     ];
   } else {
     return [
-      { name: 'Videasy (VF)', url: `https://player.videasy.net/tv/${tmdbId}/${season}/${episode}?language=fr` },
-      { name: 'VidSrc Pro',   url: `https://vidsrc.pro/embed/tv/${tmdbId}/${season}/${episode}?lang=fr` },
-      { name: 'VidSrc.to',    url: `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}?lang=fr` },
-      { name: '2Embed',       url: `https://www.2embed.cc/embedtv/${tmdbId}&s=${season}&e=${episode}&lang=fr` },
-      { name: 'Embed.su',     url: `https://embed.su/embed/tv/${tmdbId}/${season}/${episode}` },
-      { name: 'MovieBox',     url: `https://moviesapi.club/tv/${tmdbId}-${season}-${episode}` },
-      { name: 'SuperEmbed',   url: `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1&s=${season}&e=${episode}` },
-      { name: 'AutoEmbed',    url: `https://autoembed.co/tv/tmdb/${tmdbId}-${season}-${episode}` },
+      { name: 'Frembed (VF)',   url: `https://frembed.cyou/embed/tv/${tmdbId}/${season}/${episode}` },
+      { name: 'Videasy (VF)',   url: `https://player.videasy.net/tv/${tmdbId}/${season}/${episode}?language=fr` },
+      { name: 'VidSrc.me (VF)', url: `https://vidsrc.me/embed/tv?tmdb=${tmdbId}&sea=${season}&epi=${episode}&ds_lang=fr` },
+      { name: 'VidSrc Pro',     url: `https://vidsrc.pro/embed/tv/${tmdbId}/${season}/${episode}?lang=french` },
+      { name: 'VidSrc.to',      url: `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}?lang=french` },
+      { name: '2Embed',         url: `https://www.2embed.cc/library/tv/${tmdbId}?s=${season}&e=${episode}&lang=fr` },
+      { name: 'Embed.su',       url: `https://embed.su/embed/tv/${tmdbId}/${season}/${episode}?lang=french` },
+      { name: 'MovieBox',       url: `https://moviesapi.club/tv/${tmdbId}-${season}-${episode}` },
+      { name: 'SuperEmbed',     url: `https://multiembed.mov/directstream.php?video_id=${tmdbId}&tmdb=1&s=${season}&e=${episode}&lang=french` },
+      { name: 'AutoEmbed',      url: `https://autoembed.co/tv/tmdb/${tmdbId}-${season}-${episode}` },
     ];
   }
 }
